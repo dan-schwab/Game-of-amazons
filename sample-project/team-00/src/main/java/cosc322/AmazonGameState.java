bea450e17ce06adb3bb314b8b99a6c0cf41fd642
Daniel Schwab <dan.schwab3@gmail.com>
2019-03-18 20:22:36 -0700

    indexed state

--- sample-project/team-00/src/main/java/cosc322/AmazonGameState.java
+++ sample-project/team-00/src/main/java/cosc322/AmazonGameState.java
@@ -7,6 +7,8 @@ public class AmazonGameState {
 	
 	short[][] board = new short[10][10];
 	int turnNumber = 0;
+	ArrayList<short[]> blackQueens;
+	ArrayList<short[]> whiteQueens;
 	
 	
 	//0 = empty, 1 = black queen, 2 = white queen, 3 = arrow;
@@ -19,6 +21,14 @@ public class AmazonGameState {
 	public AmazonGameState(int turnNumber) {
 		this.board = new short[][]{{0,0,0,1,0,1,0,0,0,0},{0,0,0,0,0,0,0,0,0,0},{0,0,0,0,0,0,0,0,0,0},{1,0,0,0,0,0,0,0,0,1},{0,0,0,0,0,0,0,0,0,0},{0,0,0,0,0,0,0,0,0,0},{2,0,0,0,0,0,0,0,0,2},{0,0,0,0,0,0,0,0,0,0},{0,0,0,0,0,0,0,0,0,0},{0,0,0,2,0,2,0,0,0,0}};
 		this.turnNumber = turnNumber;
+		blackQueens.add({6,0});
+		blackQueens.add({9,3});
+		blackQueens.add({9,6});
+		blackQueens.add({6,9});
+		whiteQueens.add({3,0});
+		whiteQueens.add({0,3});
+		whiteQueens.add({0,6});
+		whiteQueens.add({3,9});
 	}
 	
 	
@@ -27,6 +37,15 @@ public class AmazonGameState {
 		board[QCurr.get(0)][QCurr.get(1)] = 0;
 		board[QNew.get(0)][QNew.get(1)] = queenType;
 		board[ANew.get(0)][ANew.get(1)] = 3;
+		
+		int i = blackQueens.indexOf({QCurr.get(0),QCurr.get(1)});
+		if(i>=0){
+			blackQueens.set(i, {QNew.get(0),QNew.get(1)});
+		}
+		else{
+			i = whiteQueens.indexOf({QCurr.get(0),QCurr.get(1)});
+			whiteQueens.set(i, {QNew.get(0),QNew.get(1)});
+		}
 		//check if legal move, and return false if not?
 		return true;
 	}
--- sample-project/team-00/src/main/java/cosc322/GameStateNode.java
+++ sample-project/team-00/src/main/java/cosc322/GameStateNode.java
@@ -9,10 +9,19 @@ public class GameStateNode {
 	public int turnNumber;
 	public int player;
 	public ArrayList<GameStateNode> children;
+	public short[][] territory;
 	
 	
 	public GameStateNode(AmazonGameState board, int turnNumber) {
+		this.nodeBoard = board;
+		this.turnNumber = turnNumber;
+		this.territory = new short[][];
+	}
+	
+	private void getTerritory() {
 		
 	}
+	
+	private void getMinMoves
 
 }
