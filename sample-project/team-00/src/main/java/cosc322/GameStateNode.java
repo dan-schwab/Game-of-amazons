b200e351f3d4f8f17aec70781981344768813c1a
Trevor Gallicano <trevorg@hotmail.ca>
2019-03-21 13:17:50 -0700

    Fixed GameStateNode compilation
    getMiniMoves and expanding methods no longer have compilation errors.

--- sample-project/team-00/src/main/java/cosc322/GameStateNode.java
+++ sample-project/team-00/src/main/java/cosc322/GameStateNode.java
@@ -25,157 +25,156 @@ public class GameStateNode {
 	private void getTerritory() {
 
 	}
-/*
-	private void getMinMoves() {
-		for (short i = 0; i < 4; i++) {
-			expandLeft(0, nodeBoard.blackQueens.get(i), true);
-			expandRight(0, nodeBoard.blackQueens.get(i), true);
-			expandUp(0, nodeBoard.blackQueens.get(i), true);
-			expandDown(0, nodeBoard.blackQueens.get(i), true);
-			expandDiagRightUp(0, nodeBoard.blackQueens.get(i), true);
-			expandDiagRightDown(0, nodeBoard.blackQueens.get(i), true);
-			expandDiagLeftUp(0, nodeBoard.blackQueens.get(i), true);
-			expandDiagLeftDown(0, nodeBoard.blackQueens.get(i), true);
-
-			expandLeft(0, nodeBoard.whiteQueens.get(i), false);
-			expandRight(0, nodeBoard.whiteQueens.get(i), false);
-			expandUp(0, nodeBoard.whiteQueens.get(i), false);
-			expandDown(0, nodeBoard.whiteQueens.get(i), false);
-			expandDiagRightUp(0, nodeBoard.whiteQueens.get(i), false);
-			expandDiagRightDown(0, nodeBoard.whiteQueens.get(i), false);
-			expandDiagLeftUp(0, nodeBoard.whiteQueens.get(i), false);
-			expandDiagLeftDown(0, nodeBoard.whiteQueens.get(i), false);
+        
+        private void getMinMoves(){
+		for(short i=0; i<4; i++){
+			expandLeft((short) 0, nodeBoard.blackQueens.get(i), true);
+			expandRight((short) 0, nodeBoard.blackQueens.get(i), true);
+			expandUp((short) 0, nodeBoard.blackQueens.get(i), true);
+			expandDown((short) 0, nodeBoard.blackQueens.get(i), true);
+			expandDiagRightUp((short) 0, nodeBoard.blackQueens.get(i), true);
+			expandDiagRightDown((short) 0, nodeBoard.blackQueens.get(i), true);
+			expandDiagLeftUp((short) 0, nodeBoard.blackQueens.get(i), true);
+			expandDiagLeftDown((short) 0, nodeBoard.blackQueens.get(i), true);
+			
+			expandLeft((short) 0, nodeBoard.whiteQueens.get(i), false);
+			expandRight((short) 0, nodeBoard.whiteQueens.get(i), false);
+			expandUp((short) 0, nodeBoard.whiteQueens.get(i), false);
+			expandDown((short) 0, nodeBoard.whiteQueens.get(i), false);
+			expandDiagRightUp((short) 0, nodeBoard.whiteQueens.get(i), false);
+			expandDiagRightDown((short) 0, nodeBoard.whiteQueens.get(i), false);
+			expandDiagLeftUp((short) 0, nodeBoard.whiteQueens.get(i), false);
+			expandDiagLeftDown((short) 0, nodeBoard.whiteQueens.get(i), false);
 		}
 	}
-
-	private void expandLeft(short value, short[] position, boolean black) {
-		for (short i = position[1] - 1; i >= 0; i--) {
-			if (black) {
-				if (nodeBoard.board[position[0]][i] == 0 && minBlack[position[0]][i] == 0) {
-					minBlack[position[0]][i] = value + 1;
+	
+	private void expandLeft(short value, short[] position, boolean black){
+		for(int i=position[1]-1; i>=0; i--){
+			if(black){
+				if(nodeBoard.board[position[0]][i]==0 && minBlack[position[0]][i]==0){
+					minBlack[position[0]][i] = (short) (value + (short)1);
 				}
-			} else {
-				if (nodeBoard.board[position[0]][i] == 0 && minWhite[position[0]][i] == 0) {
-					minWhite[position[0]][i] = value + 1;
+			}
+			else{
+				if(nodeBoard.board[position[0]][i]==0 && minWhite[position[0]][i]==0){
+					minWhite[position[0]][i] = (short)(value+(short)1);
 				}
 			}
 		}
 	}
-
-	private void epxandRight(short value, short[] position, boolean black) {
-		for (short i = position[1] + 1; i <= 9; i++) {
-			if (black) {
-				if (nodeBoard.board[position[0]][i] == 0 && minBlack[position[0]][i] == 0) {
-					minBlack[position[0]][i] = value + 1;
+	
+	private void expandRight(short value, short[] position, boolean black){
+		for(int i=position[1]+1; i<=9; i++){
+			if(black){
+				if(nodeBoard.board[position[0]][i]==0 && minBlack[position[0]][i]==0){
+					minBlack[position[0]][i] = (short) (value + (short)1);
 				}
-			} else {
-				if (nodeBoard.board[position[0]][i] == 0 && minWhite[position[0]][i] == 0) {
-					minWhite[position[0]][i] = value + 1;
+			}
+			else{
+				if(nodeBoard.board[position[0]][i]==0 && minWhite[position[0]][i]==0){
+					minWhite[position[0]][i] = (short)(value+(short)1);
 				}
 			}
 		}
 	}
-
-	private void expandUp(short value, short[] position, boolean black) {
-		for (short i = position[0] + 1; i <= 9; i++) {
-			if (black) {
-				if (nodeBoard.board[i][position[1]] == 0 && minBlack[i][position[1]] == 0) {
-					minBlack[i][position[1]] = value + 1;
+	
+	private void expandUp(short value, short[] position, boolean black){
+		for(int i=position[0]+1; i<=9; i++){
+			if(black){
+				if(nodeBoard.board[i][position[1]]==0 && minBlack[i][position[1]]==0){
+					minBlack[i][position[1]] = (short) (value + (short)1);
 				}
-			} else {
-				if (nodeBoard.board[i][position[1]] == 0 && minWhite[i][position[1]] == 0) {
-					minWhite[i][position[1]] = value + 1;
+			}
+			else{
+				if(nodeBoard.board[i][position[1]]==0 && minWhite[i][position[1]]==0){
+					minWhite[i][position[1]] = (short) (value + (short)1);
 				}
 			}
 		}
 	}
-
-	private void expandDown(short value, short[] position, boolean black) {
-		for (short i = position[0] - 1; i >= 0; i--) {
-			if (black) {
-				if (nodeBoard.board[i][position[1]] == 0 && minBlack[i][position[1]] == 0) {
-					minBlack[i][position[1]] = value + 1;
+	
+	private void expandDown(short value, short[] position, boolean black){
+		for(int i=position[0]-1; i>=0; i--){
+			if(black){
+				if(nodeBoard.board[i][position[1]]==0 && minBlack[i][position[1]]==0){
+					minBlack[i][position[1]] = (short) (value + (short)1);
 				}
-			} else {
-				if (nodeBoard.board[i][position[1]] == 0 && minWhite[i][position[1]] == 0) {
-					minWhite[i][position[1]] = value + 1;
+			}
+			else{
+				if(nodeBoard.board[i][position[1]]==0 && minWhite[i][position[1]]==0){
+					minWhite[i][position[1]] = (short) (value + (short)1);
 				}
 			}
 		}
 	}
-
-	private void expandDiagRightUp(short value, short[] position, boolean black) {
-		short i = 1;
-		while (position[0] + i <= 9 && position[1] + i <= 9) {
-			if (black) {
-				if (nodeBoard.board[position[0] + i][position[1] + i] == 0
-						&& minBlack[position[0] + i][position[1] + i] == 0) {
-					minBlack[position[0] + i][position[1] + i] = value + 1;
-				}
-			} else {
-				if (nodeBoard.board[position[0] + i][position[1] + i] == 0
-						&& minWhite[position[0] + i][position[1] + i] == 0) {
-					minWhite[position[0] + i][position[1] + i] = value + 1;
+	
+	private void expandDiagRightUp(short value, short[] position, boolean black){
+		short i=1;
+		while(position[0]+i<=9 && position[1]+i<=9){
+			if(black){
+				if(nodeBoard.board[position[0]+i][position[1]+i]==0 && minBlack[position[0]+i][position[1]+i]==0){
+					minBlack[position[0]+i][position[1]+i] = (short) (value + (short)1);
 				}
 			}
-			i++;
-		}
-	}
-
-	private void expandDiagRightDown(short value, short[] position, boolean black) {
-		short i = 1;
-		while (position[0] - i >= 0 && position[1] + i <= 9) {
-			if (black) {
-				if (nodeBoard.board[position[0] - i][position[1] + i] == 0
-						&& minBlack[position[0] + i][position[1] + i] == 0) {
-					minBlack[position[0] - i][position[1] + i] = value + 1;
-				}
-			} else {
-				if (nodeBoard.board[position[0] - i][position[1] + i] == 0
-						&& minWhite[position[0] - i][position[1] + i] == 0) {
-					minWhite[position[0] - i][position[1] + i] = value + 1;
+			else{
+				if(nodeBoard.board[position[0]+i][position[1]+i]==0 && minWhite[position[0]+i][position[1]+i]==0){
+					minWhite[position[0]+i][position[1]+i] = (short) (value + (short)1);
 				}
 			}
 			i++;
 		}
 	}
-
-	private void expandDiagLeftUp(short value, short[] position, boolean black) {
-		short i = 1;
-		while (position[0] + i <= 9 && position[1] - i >= 0) {
-			if (black) {
-				if (nodeBoard.board[position[0] + i][position[1] - i] == 0
-						&& minBlack[position[0] + i][position[1] - i] == 0) {
-					minBlack[position[0] + i][position[1] - i] = value + 1;
+	
+	private void expandDiagRightDown(short value, short[] position, boolean black){
+		short i=1;
+		while(position[0]-i>=0 && position[1]+i<=9){
+			if(black){
+				if(nodeBoard.board[position[0]-i][position[1]+i]==0 && minBlack[position[0]+i][position[1]+i]==0){
+					minBlack[position[0]-i][position[1]+i] = (short) (value + (short)1);
 				}
-			} else {
-				if (nodeBoard.board[position[0] + i][position[1] - i] == 0
-						&& minWhite[position[0] + i][position[1] - i] == 0) {
-					minWhite[position[0] + i][position[1] - i] = value + 1;
+			}
+			else{
+				if(nodeBoard.board[position[0]-i][position[1]+i]==0 && minWhite[position[0]-i][position[1]+i]==0){
+					minWhite[position[0]-i][position[1]+i] = (short) (value + (short)1);
 				}
 			}
 			i++;
 		}
 	}
-
-	private void expandDiagLeftDown(short value, short[] position, boolean black) {
-		short i = 1;
-		while (position[0] - i >= 0 && position[1] - i >= 0) {
-			if (black) {
-				if (nodeBoard.board[position[0] - i][position[1] - i] == 0
-						&& minBlack[position[0] - i][position[1] - i] == 0) {
-					minBlack[position[0] - i][position[1] - i] = value + 1;
+	
+	private void expandDiagLeftUp(short value, short[] position, boolean black){
+		short i=1;
+		while(position[0]+i<=9 && position[1]-i>=0){
+			if(black){
+				if(nodeBoard.board[position[0]+i][position[1]-i]==0 && minBlack[position[0]+i][position[1]-i]==0){
+					minBlack[position[0]+i][position[1]-i] = (short) (value + (short)1);
 				}
-			} else {
-				if (nodeBoard.board[position[0] - i][position[1] - i] == 0
-						&& minWhite[position[0] - i][position[1] - i] == 0) {
-					minWhite[position[0] - i][position[1] - i] = value + 1;
+			}
+			else{
+				if(nodeBoard.board[position[0]+i][position[1]-i]==0 && minWhite[position[0]+i][position[1]-i]==0){
+					minWhite[position[0]+i][position[1]-i] = (short) (value + (short)1);
 				}
 			}
 			i++;
 		}
 	}
-*/
+	
+	private void expandDiagLeftDown(short value, short[] position, boolean black){
+		short i=1;
+		while(position[0]-i>=0 && position[1]-i>=0){
+			if(black){
+				if(nodeBoard.board[position[0]-i][position[1]-i]==0 && minBlack[position[0]-i][position[1]-i]==0){
+					minBlack[position[0]-i][position[1]-i] = (short) (value + (short)1);
+				}
+			}
+			else{
+				if(nodeBoard.board[position[0]-i][position[1]-i]==0 && minWhite[position[0]-i][position[1]-i]==0){
+					minWhite[position[0]-i][position[1]-i] = (short) (value + (short)1);
+				}
+			}
+			i++;
+		}
+	}
 
 	public ArrayList<GameStateNode>	createChildren() {
 		
