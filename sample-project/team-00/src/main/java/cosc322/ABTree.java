e419ec4841bb0061266f4acecb7cb8c1e1362474
mattds9 <mattdeleeuw@hotmail.com>
2019-03-21 20:42:20 -0700

    formatted

--- sample-project/team-00/src/main/java/cosc322/ABTree.java
+++ sample-project/team-00/src/main/java/cosc322/ABTree.java
@@ -4,70 +4,62 @@ import java.util.ArrayList;
 
 public class ABTree {
 
-	public GameStateNode rootNode;
-	int depth = 0;
-	ArrayList<GameStateNode> searchSpace = new ArrayList<GameStateNode>();
+    public GameStateNode rootNode;
+    int depth = 0;
+    ArrayList<GameStateNode> searchSpace = new ArrayList<GameStateNode>();
 
+    public ABTree(GameStateNode root) {
+        this.rootNode = root;
+    }
 
-	public ABTree(GameStateNode root) {
-		this.rootNode = root;
-	}
+    public int ABSearch(GameStateNode node, int depth, int alpha, int beta) {
 
-
-	public int ABSearch(GameStateNode node, int depth, int alpha, int beta) {
-
-		if(depth == 0) {
-			node.value = h_value(node);
-			return node.value;
-		}
-
-		for(GameStateNode child : node.children) {
-
-			int currentScore = ABSearch(child, depth -1, alpha, beta);
-
-			if(node.asBlack) {
-			alpha = Math.max(alpha, currentScore);
-			if(alpha >= beta) break;
-			}
-			else {
-				beta = Math.min(beta, currentScore);
-				if(beta <= alpha) break;
-			}
-
-		}
-
-		if(node.asBlack) {
-			node.value = alpha;
-		}
-		else {
-			node.value = beta;
-		}
-		return node.value;
-	}
-
-	public int h_value(GameStateNode node) {
-		return 0;
-
-	}
-
-
-	public void createFrontier() {
-
-		rootNode.createChildren();
-
-
-	}
-
-	public int h_value(GameStateNode node) {
-
-
-            return 0;
-	}
-
-        public int miniMax(GameStateNode node, boolean isMaxPlayer, int alpha, int beta){
-            //if node is leaf: return value of the node
-            int bestVal = 0;
-            return bestVal;
+        if (depth == 0) {
+            node.value = h_value(node);
+            return node.value;
         }
+
+        for (GameStateNode child : node.children) {
+
+            int currentScore = ABSearch(child, depth - 1, alpha, beta);
+
+            if (node.asBlack) {
+                alpha = Math.max(alpha, currentScore);
+                if (alpha >= beta) {
+                    break;
+                }
+            } else {
+                beta = Math.min(beta, currentScore);
+                if (beta <= alpha) {
+                    break;
+                }
+            }
+
+        }
+
+        if (node.asBlack) {
+            node.value = alpha;
+        } else {
+            node.value = beta;
+        }
+        return node.value;
+    }
+
+    public int h_value(GameStateNode node) {
+        return 0;
+
+    }
+
+    public void createFrontier() {
+
+        rootNode.createChildren();
+
+    }
+
+    public int miniMax(GameStateNode node, boolean isMaxPlayer, int alpha, int beta) {
+        //if node is leaf: return value of the node
+        int bestVal = 0;
+        return bestVal;
+    }
 
 }
